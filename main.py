3ff4252625ec80b5b7bf984d2b4f851ef3561d29
바북이 <60121472+babuk2@users.noreply.github.com>
2024-12-31 13:15:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+@app.get("/test", response_class=HTMLResponse)
+async def read_root():
+    return """
+    <html>
+        <body>
+            <h1>되는거냐?!</h1>
+        </body>
+    </html>
+    """
